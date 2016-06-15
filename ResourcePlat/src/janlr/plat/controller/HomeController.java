5776af9d3735c21bda03275e1a525e92130ec45a
Jeremy <361600134@qq.com>
2016-06-16 01:22:01 +0800

    框架

---
+++ ResourcePlat/src/janlr/plat/controller/HomeController.java
@@ -0,0 +1,27 @@
+package janlr.plat.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+@RequestMapping("admin")
+public class HomeController {
+
+	/***
+	 * yourProjectName/admin/home
+	 * @return
+	 */
+	@RequestMapping(value = "index", method = RequestMethod.GET)
+	public ModelAndView index() {
+		ModelAndView mav = new ModelAndView("home");
+		return mav;
+	}
+	
+}
+
+
+
+
+
